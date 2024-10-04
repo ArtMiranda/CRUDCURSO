59dfd3494d399be494965c42a0b19993982b5cdd
Artur Miranda <artcamir12@gmail.com>
2024-10-04 11:47:34 -0300

    feat: activity de dados

--- app/src/main/java/br/dev/arturmiranda/crudcurso/MainActivity.kt
+++ app/src/main/java/br/dev/arturmiranda/crudcurso/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import br.dev.arturmiranda.crudcurso.adapter.CursoAdapter
 import br.dev.arturmiranda.crudcurso.cursoDB.CursoDAO
 import br.dev.arturmiranda.crudcurso.model.Curso
@@ -39,6 +40,11 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AddCursoActivity::class.java)
             startActivity(intent)
         }
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Cursos Cadastrados"
+
     }
 
     override fun onResume() {
